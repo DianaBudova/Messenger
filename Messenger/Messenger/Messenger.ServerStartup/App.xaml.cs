5c336367f44b8c0ed02e42019c80dfd493c8850b
Budova Diana <dianachampions011@gmail.com>
2023-08-03 21:48:15 +0300

    Changes for 03.08.2023 21:47. Remote connection to database works.

--- Messenger/Messenger/Messenger.ServerStartup/App.xaml.cs
+++ Messenger/Messenger/Messenger.ServerStartup/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using Messenger.DAL;
+using Messenger.Models.DB;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Configuration;
+using System.Linq;
+using System.Windows;
 
 namespace Messenger.ServerStartup
 {
@@ -7,5 +13,22 @@ namespace Messenger.ServerStartup
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            using DataContext context = new();
+            if (!(context.Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator)!.Exists())
+                context.Database.EnsureCreated();
+            this.ConfigureTables(context);
+        }
+
+        private void ConfigureTables(DataContext context)
+        {
+            if (context.Server.Any())
+                return;
+            string? serverNameByDefault = ConfigurationManager.AppSettings["ServerNameByDefault"];
+            if (serverNameByDefault is null)
+                return;
+            context.Server.Add(Server.DefaultServer).Context.SaveChanges();
+        }
     }
 }
